624baee061acd2d22a1ef4e0d109b2aaab2114fe
Adam <afranklint16@gmail.com>
2018-05-15 09:56:07 -0700

    Add bStats
    I don't know why

--- src/main/java/com/github/pocketkid2/mvp/MVPlugin.java
+++ src/main/java/com/github/pocketkid2/mvp/MVPlugin.java
@@ -1,11 +1,13 @@
 package com.github.pocketkid2.mvp;
 
+import org.bstats.bukkit.MetricsLite;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MVPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		MetricsLite metrics = new MetricsLite(this);
 		getLogger().info("Done!");
 	}
 
